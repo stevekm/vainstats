2a7c36d0f470894125534a51f642a420ceaa0732
Stephen Kelly <stevekm@users.noreply.github.com>
2017-06-22 21:40:34 -0400

    refactor for module importing

--- get_stats.py
+++ get_stats.py
@@ -416,19 +416,10 @@ def fail_finder(user_data):
     for key, data in OrderedDict(sorted(rankings.items(), key=lambda x: x[1]['total'], reverse=True)).items():
         print(row_format.format(data['name'], data['hero'], data['team'], str(data['total']), key))
 
-def run(args):
+def main(username = None, api_key_file = 'key.txt', region = 'na', match_ID = None,  days = 1, debug_mode = False, page_limit = 3, i_mode = False, harvest_mode = False, fail_mode = False):
     '''
+    Main control function for the script
     '''
-    username = args.name
-    api_key_file = args.api_key_file
-    region = args.region
-    match_ID = args.match
-    days = args.days
-    debug_mode = args.debug_mode
-    page_limit = args.pages
-    i_mode = args.i_mode
-    harvest_mode = args.harvest_mode
-    fail_mode = args.fail_mode
     print('Player name: {0}'.format(username))
     print('Region: {0}'.format(get_region_name(region = region)))
     key = get_api_key(api_key_file)
@@ -436,25 +427,39 @@ def run(args):
     match_url = build_match_url(region, match_ID)
     get_match_data(username = username, key = key, match_url = match_url, match_ID = match_ID, days_to_subtract = days, page_limit = page_limit, debug_mode = debug_mode, i_mode = i_mode, harvest_mode = harvest_mode, fail_mode = fail_mode)
 
+def run():
+    '''
+    Arg parsing for the script when run from command line
+    '''
+    parser = argparse.ArgumentParser(description='Vainglory Player Match Stats')
+    # required flags
 
+    # optional flags
+    parser.add_argument("-n", default = None, type = str,  dest = 'username', metavar = 'name', help="Player's in-game username")
+    parser.add_argument("-d", default = 1, type = int,  dest = 'days', metavar = 'days', help="Number of past days in which to search for matches")
+    parser.add_argument("-p", default = 3, type = int,  dest = 'page_limit', metavar = 'page limit', help="'Page Limit'; number of matches to return")
+    parser.add_argument("-m", default = None, type = str, dest = 'match_ID', metavar = 'match', help="Match ID to look up")
+    parser.add_argument("-k", default = 'key.txt', type = str, dest = 'api_key_file', metavar = 'api_key_file', help="Path to text file containing the player's API key. (get one here: https://developer.vainglorygame.com/)")
+    parser.add_argument("-r", default = 'na', type = str, dest = 'region', metavar = 'region', help="Player's region. Possibilties: na, eu, sa, ea, or sg. Details here: https://developer.vainglorygame.com/docs?python#regions")
+    parser.add_argument("--debug", default = False, action='store_true', dest = 'debug_mode', help="Print the query command to console, so you can copy/paste the code elsewhere")
+    parser.add_argument("-i", "--interactive", default = False, action='store_true', dest = 'i_mode', help="Start an interactive Python session after querying match data")
+    parser.add_argument("--harvest", default = False, action='store_true', dest = 'harvest_mode', help="'Harvest' mode, saves each match to a JSON file")
+    parser.add_argument("--fail", default = False, action='store_true', dest = 'fail_mode', help="'Fail Finder' mode, ranks players in a match (match ID required)")
 
-# ~~~~ GET SCRIPT ARGS ~~~~~~ #
-parser = argparse.ArgumentParser(description='Vainglory Player Match Stats')
-# required flags
+    args = parser.parse_args()
 
-# optional flags
-parser.add_argument("-n", default = None, type = str,  dest = 'name', metavar = 'name', help="Player's in-game username")
-parser.add_argument("-d", default = 1, type = int,  dest = 'days', metavar = 'days', help="Number of past days in which to search for matches")
-parser.add_argument("-p", default = 3, type = int,  dest = 'pages', metavar = 'page limit', help="'Page Limit'; number of matches to return")
-parser.add_argument("-m", default = None, type = str, dest = 'match', metavar = 'match', help="Match ID to look up")
-parser.add_argument("-k", default = 'key.txt', type = str, dest = 'api_key_file', metavar = 'api_key_file', help="Path to text file containing the player's API key. (get one here: https://developer.vainglorygame.com/)")
-parser.add_argument("-r", default = 'na', type = str, dest = 'region', metavar = 'region', help="Player's region. Possibilties: na, eu, sa, ea, or sg. Details here: https://developer.vainglorygame.com/docs?python#regions")
-parser.add_argument("--debug", default = False, action='store_true', dest = 'debug_mode', help="Print the query command to console, so you can copy/paste the code elsewhere")
-parser.add_argument("-i", "--interactive", default = False, action='store_true', dest = 'i_mode', help="Start an interactive Python session after querying match data")
-parser.add_argument("--harvest", default = False, action='store_true', dest = 'harvest_mode', help="'Harvest' mode, saves each match to a JSON file")
-parser.add_argument("--fail", default = False, action='store_true', dest = 'fail_mode', help="'Fail Finder' mode, ranks players in a match (match ID required)")
+    username = args.username
+    api_key_file = args.api_key_file
+    region = args.region
+    match_ID = args.match_ID
+    days = args.days
+    debug_mode = args.debug_mode
+    page_limit = args.page_limit
+    i_mode = args.i_mode
+    harvest_mode = args.harvest_mode
+    fail_mode = args.fail_mode
 
-args = parser.parse_args()
+    main(username = username, api_key_file = api_key_file, region = region, match_ID = match_ID,  days = days, debug_mode = debug_mode, page_limit = page_limit, i_mode = i_mode, harvest_mode = harvest_mode, fail_mode = fail_mode)
 
 if __name__ == "__main__":
-    run(args)
+    run()
