742e084739ee1d6eff6620762c3e3c4999579eaa
Stephen Kelly <stevekm@users.noreply.github.com>
2017-07-10 20:31:44 -0400

    add demo gold plot

--- app.py
+++ app.py
@@ -36,6 +36,8 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 import plotly.graph_objs as go
+import plotly.plotly as py
+# import plotly.offline
 import pandas as pd
 
 # app modules
@@ -72,7 +74,7 @@ app.layout = html.Div([
     html.H4(children='Match Stats'),
     html.Div(id = 'roster-table'),
     html.Div([
-        # dcc.Graph(id='demo-roster-gold-plot', animate=True),
+        dcc.Graph(id='demo-roster-gold-plot', animate=True),
     ]),
 
     # third sub div
@@ -96,6 +98,39 @@ app.layout = html.Div([
 
 
 # ~~~~ APP SERVER ~~~~~~ #
+def make_demo_roster_df(match_id):
+    '''
+    Return a df for the roster of a given match
+    '''
+    logger.debug("Match id: {0}".format(match_id))
+    logger.debug("Retreiving specified match from data set")
+    match = vp.get_match(data = vd.demo_data, match_id = match_id)
+    logger.debug("Getting roster ids for the match")
+    rosters_ids = vp.get_roster_ids(match = match)
+    logger.debug("Roster ids: {0}".format(rosters_ids))
+    logger.debug("Getting rosters for the match")
+    rosters = vp.get_rosters(roster_ids = rosters_ids, data = vd.demo_data)
+    for item in rosters:
+        logger.debug(item)
+    logger.debug("Making roster df")
+    roster_df_list = [pd.DataFrame.from_dict(item['attributes']['stats'], orient='index') for item in rosters]
+    vd.roster_df = pd.concat(roster_df_list, axis=1).transpose()
+    logger.debug(vd.roster_df)
+    return(vd.roster_df)
+
+def make_api_roster_df(match_id):
+    '''
+    Return a df for the roster of an API quieried match
+    '''
+    logger.debug("Match id: {0}".format(match_id))
+    logger.debug("Retreiving specified match from data set")
+    match = vp.get_glmatch(data = vd.matches, match_id = match_id)
+    logger.debug("Making roster df")
+    roster_df_list = [pd.DataFrame.from_dict(item.stats, orient='index') for item in match.rosters]
+    roster_df = pd.concat(roster_df_list, axis=1).transpose()
+    logger.debug(roster_df)
+    return(roster_df)
+
 @app.callback(
     Output(component_id = 'roster-table', component_property = 'children'),
     [Input(component_id = 'dict-key', component_property = 'value')]
@@ -104,59 +139,26 @@ def update_roster_table(input_value):
     logger.debug("Updating input value: {0}".format(input_value))
     if input_value != None:
         match_id = input_value
-        logger.debug("Match id: {0}".format(match_id))
-
-        logger.debug("Retreiving specified match from data set")
-        match = vp.get_match(data = vd.demo_data, match_id = match_id)
-
-        logger.debug("Getting roster ids for the match")
-        rosters_ids = vp.get_roster_ids(match = match)
-        logger.debug("Roster ids: {0}".format(rosters_ids))
-
-        logger.debug("Getting rosters for the match")
-        rosters = vp.get_rosters(roster_ids = rosters_ids, data = vd.demo_data)
-        for item in rosters:
-            logger.debug(item)
-
-        logger.debug("Making roster df")
-        roster_df_list = [pd.DataFrame.from_dict(item['attributes']['stats'], orient='index') for item in rosters]
-        vd.roster_df = pd.concat(roster_df_list, axis=1).transpose()
-
-        logger.debug(vd.roster_df)
-
+        vd.roster_df = make_demo_roster_df(match_id = match_id)
         return(vt.html_df_table(df = vd.roster_df))
     else:
         return('No match selected')
 
-# @app.callback(
-#     Output(component_id = 'demo-roster-gold-plot', component_property = 'figure'),
-#     [Input(component_id = 'dict-key', component_property = 'value')]
-# )
-# def update_demo_roster_gold_plot(input_value):
-#     logger.debug("Updating input value: {0}".format(input_value))
-#     if input_value != None:
-#         match_id = input_value
-#         logger.debug("Match id: {0}".format(match_id))
-#
-#         logger.debug("Retreiving specified match from data set")
-#         match = vp.get_match(data = vd.demo_data, match_id = match_id)
-#
-#         logger.debug("Getting roster ids for the match")
-#         rosters_ids = vp.get_roster_ids(match = match)
-#         logger.debug("Roster ids: {0}".format(rosters_ids))
-#
-#         logger.debug("Getting rosters for the match")
-#         rosters = vp.get_rosters(roster_ids = rosters_ids, data = vd.demo_data)
-#         for item in rosters:
-#             logger.debug(item)
-#
-#         logger.debug("Making roster df")
-#         roster_df_list = [pd.DataFrame.from_dict(item['attributes']['stats'], orient='index') for item in rosters]
-#         vd.roster_df = pd.concat(roster_df_list, axis=1).transpose()
-#
-#         logger.debug(vd.roster_df)
-#     else:
-#         return('No match selected')
+@app.callback(
+    Output(component_id = 'demo-roster-gold-plot', component_property = 'figure'),
+    [Input(component_id = 'dict-key', component_property = 'value')]
+)
+def update_demo_roster_gold_plot(input_value):
+    logger.debug("Updating input value: {0}".format(input_value))
+    if input_value != None:
+        match_id = input_value
+        vd.roster_df = make_demo_roster_df(match_id = match_id)
+        return({
+        'data': [go.Bar(x = vd.roster_df['side'], y = vd.roster_df['gold'])]
+        })
+    else:
+        return('No match selected')
+
 
 
 @app.callback(
@@ -167,15 +169,7 @@ def update_api_roster_table(input_value):
     if input_value != None:
         logger.debug("Updating selected-api-match-id value: {0}".format(input_value))
         match_id = input_value
-        logger.debug("Match id: {0}".format(match_id))
-        logger.debug("Retreiving specified match from data set")
-        match = vp.get_glmatch(data = vd.matches, match_id = match_id)
-        logger.debug("Making roster df")
-        roster_df_list = [pd.DataFrame.from_dict(item.stats, orient='index') for item in match.rosters]
-        roster_df = pd.concat(roster_df_list, axis=1).transpose()
-
-        logger.debug(roster_df)
-
+        roster_df = make_api_roster_df(match_id = match_id)
         return(vt.html_df_table(df = roster_df))
     else:
         return('No match selected')
